5f373ded27df7dd26bf35044f69917f29855f9e9
RyosukeNakamoto <natorike.0902@gmail.com>
2019-02-13 18:01:05 +0900

    Add Unity

---
+++ Satellite/Assets/Scripts/Scenecontroller.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenecontroller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //タイトル画面でボタンを押したときホーム画面に遷移
+    public void OnClickTitle()
+    {
+        SceneManager.LoadScene("Home");
+    }
+
+    //ホーム画面でボタンを押したときにステージセレクト画面に遷移
+    public void OnClickHome()
+    {
+        SceneManager.LoadScene("Stageselect");
+    }
+
+    //ホーム画面でボタンを押したときにタイトル画面に遷移
+    public void OnClickTitlereturn()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
+    //ステージセレクト画面でボタンを押したときホーム画面に遷移
+    public void OnClickHomereturn()
+    {
+        SceneManager.LoadScene("Home");
+    }
+}
