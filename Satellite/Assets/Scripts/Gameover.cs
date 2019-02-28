c0db45c10f32bca9e6cea1d35d4b73630ca4cc01
RyosukeNakamoto <natorike.0902@gmail.com>
2019-02-28 19:10:53 +0900

    Add Background

---
+++ Satellite/Assets/Scripts/Gameover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Gameover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = 0.0f;
+
+        Scene loadscene = SceneManager.GetActiveScene();
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene(loadscene.name);
+        }
+    }
+}
