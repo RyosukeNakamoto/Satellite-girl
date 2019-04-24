8b4108ae68cfcbe0a5e6e94263afc31f19a133a7
RyosukeNakamoto <natorike.0902@gmail.com>
2019-04-24 23:19:49 +0900

    Add StageRecognitionImage

--- Satellite/Assets/Scenes/Scripts/CharacterSelect/CharacterSelect.cs
+++ Satellite/Assets/Scenes/Scripts/CharacterSelect/CharacterSelect.cs
@@ -7,7 +7,7 @@ public class CharacterSelect : MonoBehaviour
 
     public Canvas[] character;
 
-    int selectNumber = 0;
+    bool characterSelect = true;
 
     // Start is called before the first frame update
     void Start()
@@ -15,35 +15,26 @@ public class CharacterSelect : MonoBehaviour
         //シーン開始時の表示、非表示
         character[0].enabled = true;
         character[1].enabled = false;
-        character[2].enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         //キーを押したときにキャラクター切り替え
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.S))
         {
-            selectNumber++;
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            selectNumber--;
-        }
-
-        //selectNumberが2を越えた時、0に戻す
-        if (selectNumber > 2)
-        {
-            selectNumber = 0;
-        }
-        //selectNumberが0を下回ったとき、2にする
-        if (selectNumber < 0)
-        {
-            selectNumber = 2;
+            if (characterSelect == true)
+            {
+                characterSelect = false;
+            }
+            else
+            {
+                characterSelect = true;
+            }
         }
 
         //一号機セレステル選択状態
-        if (selectNumber == 0)
+        if (characterSelect == true)
         {
             character[0].enabled = true;
         }
@@ -52,22 +43,13 @@ public class CharacterSelect : MonoBehaviour
             character[0].enabled = false;
         }
         //二号機ファー選択状態
-        if (selectNumber == 1)
+        if (characterSelect == false)
         {
             character[1].enabled = true;
         }
         else
         {
             character[1].enabled = false;
-        }
-        //三号機ペレグリーネ選択状態
-        if (selectNumber == 2)
-        {
-            character[2].enabled = true;
-        }
-        else
-        {
-            character[2].enabled = false;
         }
     }
 }
