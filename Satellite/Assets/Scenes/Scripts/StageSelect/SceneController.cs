8a66af0ad3ec02cdd478bbab66fe1810f1ee9401
RyosukeNakamoto <natorike.0902@gmail.com>
2019-04-26 16:26:38 +0900

    Add Controller

--- Satellite/Assets/Scenes/Scripts/StageSelect/SceneController.cs
+++ Satellite/Assets/Scenes/Scripts/StageSelect/SceneController.cs
@@ -12,7 +12,7 @@ namespace Satellite.StageSelect
         [SerializeField]
         Color selectedColor = Color.white;
 
-
+        //ステージの画像
         public Image[] stage1;
         public Image[] stage2;
         public Image[] stage3;
@@ -25,19 +25,24 @@ namespace Satellite.StageSelect
         public Image[] stage10;
         public Image[] stage11;
         public Image[] stage12;
-
+        
+        //選んだステージのテキスト
         public Text[] stageImageText;
 
+        //ステージ前半、後半
         public Canvas[] stage;
 
+        //音
         AudioSource audioSource;
         public AudioClip[] sound;
 
-        //曲再生の判定
-        bool isAudioStart = false;
         //どこを選択中か数値で管理
         int selectNumber = 0;
         int stageHalf = 0;
+
+        bool stageChangeOff = false;
+        bool stageHalfChange = false;
+        bool keyInput = false;
 
         // Start is called before the first frame update
         void Start()
@@ -66,50 +71,77 @@ namespace Satellite.StageSelect
             //トリガー入力でステージの前半、後半切り替え
             float tri = Input.GetAxis("L_R_Trigger");
 
-            //右矢印キーを押したときSelectNumberを増やす
-            if (Input.GetKeyDown(KeyCode.RightArrow) || dpv > 0)
+            if (dpv == 0)
             {
-                selectNumber++;
-                //カーソル移動したとき音再生
-                audioSource.PlayOneShot(sound[0]);
+                keyInput = false;
             }
-            //左矢印キーを押したときSelectNumberを減らす
-            if (Input.GetKeyDown(KeyCode.LeftArrow) || dpv < 0)
+            if (tri == 0)
             {
-                selectNumber--;
-                //カーソル移動したとき音再生
-                audioSource.PlayOneShot(sound[0]);
+                stageHalfChange = false;
             }
-            //SelectNumberが5を越えた時0に戻す処理
-            if (selectNumber > 5)
+
+            if (stageChangeOff == false&&keyInput==false)
             {
-                selectNumber = 0;
-            }
-            //SelectNumberが0を下回った時5にする処理
-            if (selectNumber < 0)
-            {
-                selectNumber = 5;
-            }
-            //ステージの前半後半を切替
-            if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D)|| tri < 0 || tri > 0)
-            {
-                if (stageHalf == 0)
+                //右矢印キーを押したときSelectNumberを増やす
+                if (Input.GetKeyDown(KeyCode.RightArrow) || dpv > 0)
                 {
-                    stageHalf = 1;
+                    selectNumber++;
 
+                    keyInput = true;
+
+                    //カーソル移動したとき音再生
                     audioSource.PlayOneShot(sound[0]);
-
-                    stage[0].enabled = false;
-                    stage[1].enabled = true;
                 }
-                else
+                //左矢印キーを押したときSelectNumberを減らす
+                if (Input.GetKeyDown(KeyCode.LeftArrow) || dpv < 0)
                 {
-                    stageHalf = 0;
+                    selectNumber--;
 
+                    keyInput = true;
+
+                    //カーソル移動したとき音再生
                     audioSource.PlayOneShot(sound[0]);
+                }
+                //SelectNumberが5を越えた時0に戻す処理
+                if (selectNumber > 5)
+                {
+                    selectNumber = 0;
+                }
+                //SelectNumberが0を下回った時5にする処理
+                if (selectNumber < 0)
+                {
+                    selectNumber = 5;
+                }
+                if (stageHalfChange == false)
+                {
+                    //ステージの前半後半を切替
+                    if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D) || tri < 0 || tri > 0)
+                    {
+                        if (stageHalf == 0)
+                        {
+                            stageHalf = 1;
 
-                    stage[0].enabled = true;
-                    stage[1].enabled = false;
+                            //音の再生
+                            audioSource.PlayOneShot(sound[0]);
+
+                            stageHalfChange = true;
+
+                            stage[0].enabled = false;
+                            stage[1].enabled = true;
+                        }
+                        else
+                        {
+                            stageHalf = 0;
+
+                            //音の再生
+                            audioSource.PlayOneShot(sound[0]);
+
+                            stageHalfChange = true;
+
+                            stage[0].enabled = true;
+                            stage[1].enabled = false;
+                        }
+                    }
                 }
             }
 
@@ -126,18 +158,22 @@ namespace Satellite.StageSelect
                     //ステージ選択表示をステージ1に切り替え
                     stageImageText[0].enabled = true;
 
-                    //エンターキーを押したときキャラクター選択画面に遷移(プロト)
+                    //エンターキーを押したときキャラクター選択画面に遷移
                     if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown("joystick button 1"))
                     {
                         //決定時の音再生
                         audioSource.PlayOneShot(sound[1]);
 
+                        //ゲームコントローラーにステージ1を選んだ情報を渡す
                         GameController.Instance.stage = 0;
 
+                        stageChangeOff = true;
+
                         //一秒後にシーン遷移
-                        Invoke("MoveScene", 1);
+                        Invoke("MoveScene", 0.5f);
                     }
                 }
+                //Stage1を非選択状態
                 else
                 {
                     stage1[0].enabled = true;
@@ -161,12 +197,16 @@ namespace Satellite.StageSelect
                         //決定時の音再生
                         audioSource.PlayOneShot(sound[1]);
 
+                        //ゲームコントローラーにステージ2を選んだ情報を渡す
                         GameController.Instance.stage = 1;
 
+                        stageChangeOff = true;
+
                         //一秒後にシーン遷移
-                        Invoke("MoveScene", 1);
+                        Invoke("MoveScene", 0.5f);
                     }
                 }
+                //Stage2を選択状態
                 else
                 {
                     stage2[0].enabled = true;
@@ -190,12 +230,16 @@ namespace Satellite.StageSelect
                         //決定時の音再生
                         audioSource.PlayOneShot(sound[1]);
 
+                        //ゲームコントローラーにステージ3を選んだ情報を渡す
                         GameController.Instance.stage = 2;
 
+                        stageChangeOff = true;
+
                         //一秒後にシーン遷移
-                        Invoke("MoveScene", 1);
+                        Invoke("MoveScene", 0.5f);
                     }
                 }
+                //Stage3を非選択状態
                 else
                 {
                     stage3[0].enabled = true;
@@ -220,12 +264,16 @@ namespace Satellite.StageSelect
                         //決定時の音再生
                         audioSource.PlayOneShot(sound[1]);
 
+                        //ゲームコントローラーにステージ4を選んだ情報を渡す
                         GameController.Instance.stage = 3;
 
+                        stageChangeOff = true;
+
                         //一秒後にシーン遷移
-                        Invoke("MoveScene", 1);
+                        Invoke("MoveScene", 0.5f);
                     }
                 }
+                //Stage4を選択状態
                 else
                 {
                     stage4[0].enabled = true;
@@ -249,12 +297,16 @@ namespace Satellite.StageSelect
                         //決定時の音再生
                         audioSource.PlayOneShot(sound[1]);
 
+                        //ゲームコントローラーにステージ5を選んだ情報を渡す
                         GameController.Instance.stage = 4;
 
+                        stageChangeOff = true;
+
                         //一秒後にシーン遷移
-                        Invoke("MoveScene", 1);
+                        Invoke("MoveScene", 0.5f);
                     }
                 }
+                //Stage5を非選択状態
                 else
                 {
                     stage5[0].enabled = true;
@@ -278,12 +330,16 @@ namespace Satellite.StageSelect
                         //決定時の音再生
                         audioSource.PlayOneShot(sound[1]);
 
+                        //ゲームコントローラーにステージ6を選んだ情報を渡す
                         GameController.Instance.stage = 5;
 
+                        stageChangeOff = true;
+
                         //一秒後にシーン遷移
-                        Invoke("MoveScene", 1);
+                        Invoke("MoveScene", 0.5f);
                     }
                 }
+                //Stage6を非選択状態
                 else
                 {
                     stage6[0].enabled = true;
@@ -312,12 +368,16 @@ namespace Satellite.StageSelect
                         //決定時の音再生
                         audioSource.PlayOneShot(sound[1]);
 
+                        //ゲームコントローラーにステージ7を選んだ情報を渡す
                         GameController.Instance.stage = 6;
 
+                        stageChangeOff = true;
+
                         //一秒後にシーン遷移
-                        Invoke("MoveScene", 1);
+                        Invoke("MoveScene", 0.5f);
                     }
                 }
+                //Stage7を非選択状態
                 else
                 {
                     stage7[0].enabled = true;
@@ -341,12 +401,16 @@ namespace Satellite.StageSelect
                         //決定時の音再生
                         audioSource.PlayOneShot(sound[1]);
 
+                        //ゲームコントローラーにステージ8を選んだ情報を渡す
                         GameController.Instance.stage = 7;
 
+                        stageChangeOff = true;
+
                         //一秒後にシーン遷移
-                        Invoke("MoveScene", 1);
+                        Invoke("MoveScene", 0.5f);
                     }
                 }
+                //Stage8を非選択状態
                 else
                 {
                     stage8[0].enabled = true;
@@ -370,12 +434,16 @@ namespace Satellite.StageSelect
                         //決定時の音再生
                         audioSource.PlayOneShot(sound[1]);
 
+                        //ゲームコントローラーにステージ9を選んだ情報を渡す
                         GameController.Instance.stage = 8;
 
+                        stageChangeOff = true;
+
                         //一秒後にシーン遷移
-                        Invoke("MoveScene", 1);
+                        Invoke("MoveScene", 0.5f);
                     }
                 }
+                //Stage9を非選択状態
                 else
                 {
                     stage9[0].enabled = true;
@@ -400,12 +468,16 @@ namespace Satellite.StageSelect
                         //決定時の音再生
                         audioSource.PlayOneShot(sound[1]);
 
+                        //ゲームコントローラーにステージ10を選んだ情報を渡す
                         GameController.Instance.stage = 9;
 
+                        stageChangeOff = true;
+
                         //一秒後にシーン遷移
-                        Invoke("MoveScene", 1);
+                        Invoke("MoveScene", 0.5f);
                     }
                 }
+                //Stage10を非選択状態
                 else
                 {
                     stage10[0].enabled = true;
@@ -429,12 +501,16 @@ namespace Satellite.StageSelect
                         //決定時の音再生
                         audioSource.PlayOneShot(sound[1]);
 
+                        //ゲームコントローラーにステージ11を選んだ情報を渡す
                         GameController.Instance.stage = 10;
 
+                        stageChangeOff = true;
+
                         //一秒後にシーン遷移
-                        Invoke("MoveScene", 1);
+                        Invoke("MoveScene", 0.5f);
                     }
                 }
+                //Stage11を非選択状態
                 else
                 {
                     stage11[0].enabled = true;
@@ -458,12 +534,16 @@ namespace Satellite.StageSelect
                         //決定時の音再生
                         audioSource.PlayOneShot(sound[1]);
 
+                        //ゲームコントローラーにステージ12を選んだ情報を渡す
                         GameController.Instance.stage = 11;
 
+                        stageChangeOff = true;
+
                         //一秒後にシーン遷移
-                        Invoke("MoveScene", 1);
+                        Invoke("MoveScene", 0.5f);
                     }
                 }
+                //Stage12を非選択状態
                 else
                 {
                     stage12[0].enabled = true;
@@ -476,8 +556,10 @@ namespace Satellite.StageSelect
         }
 
         //シーンの遷移
-        private void MoveScene()
+        public void MoveScene()
         {
+
+            stageChangeOff = false;
 
             SceneManager.LoadScene("CharacterSelect");
 
