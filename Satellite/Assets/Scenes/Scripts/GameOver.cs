66ed8428fc4710697af8badb8a094d1e52a258f3
RyosukeNakamoto <natorike.0902@gmail.com>
2019-05-07 11:13:12 +0900

    Add BossStage

--- Satellite/Assets/Scenes/Scripts/GameOver.cs
+++ Satellite/Assets/Scenes/Scripts/GameOver.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameOver : MonoBehaviour
 {
@@ -9,12 +10,19 @@ public class GameOver : MonoBehaviour
     public AudioClip[] sound;
     // サウンドの変数
     AudioSource audioSource;
+    //選択画像を指定
+    public Image[] selectImage;
+    //選択を数値で管理
+    int selectNumber = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         // オーディオのコンポーネント
         audioSource = GetComponent<AudioSource>();
+
+        selectImage[0].color = Color.gray;
+        selectImage[1].color = Color.gray;
     }
 
     // Update is called once per frame
@@ -25,12 +33,50 @@ public class GameOver : MonoBehaviour
 
         //Scene loadscene = SceneManager.GetActiveScene();
 
-        //ゲームオーバー後、エンターキーでタイトルに戻る
-        if (Input.GetKeyDown(KeyCode.Return))
+        //十字キー縦の入力
+        float dph = Input.GetAxis("D_Pad_H");
+
+        
+        if (Input.GetKeyDown(KeyCode.UpArrow)|| dph > 0)
         {
-            SceneManager.LoadScene("Title");
-            // ゲームの時間を戻す
-            Time.timeScale = 1.0f;
+            selectNumber = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow)|| dph < 0)
+        {
+            selectNumber = 2;
+        }
+        //やり直す選択中
+        if (selectNumber == 1)
+        {
+            selectImage[0].color = Color.white;
+            //エンターキーでキャラクター選択画面に遷移
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                SceneManager.LoadScene("CharacterSelect");
+                // ゲームの時間を戻す
+                Time.timeScale = 1.0f;
+            }
+        }
+        else
+        {
+            selectImage[0].color = Color.gray;
+        }
+
+        //マップ選択に戻る選択中
+        if (selectNumber == 2)
+        {
+            selectImage[1].color = Color.white;
+            //エンターキーでステージセレクト画面に遷移
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                SceneManager.LoadScene("Stageselect");
+                // ゲームの時間を戻す
+                Time.timeScale = 1.0f;
+            }
+        }
+        else
+        {
+            selectImage[1].color = Color.gray;
         }
     }
 }
--- Satellite/Assets/Scenes/Scripts/Stage1/Clear.cs
+++ Satellite/Assets/Scenes/Scripts/Stage1/Clear.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Clear : MonoBehaviour
 {
@@ -10,11 +11,22 @@ public class Clear : MonoBehaviour
     // サウンドの変数
     AudioSource audioSource;
 
+    public Image[] selectImage;
+
+    int selectNumber = 0;
+
+    bool dphInput = false;
+
     // Start is called before the first frame update
     void Start()
     {
         // オーディオのコンポーネント
         audioSource = GetComponent<AudioSource>();
+
+        for(int i = 0; i < 3; i++)
+        {
+            selectImage[i].color = Color.gray;
+        }
     }
 
     // Update is called once per frame
@@ -24,6 +36,130 @@ public class Clear : MonoBehaviour
         Time.timeScale = 0.0f;
         Scene loadscene = SceneManager.GetActiveScene();
 
+        //十字キー縦の入力
+        float dph = Input.GetAxis("D_Pad_H");
+
+        if (dph == 0)
+        {
+            dphInput = false;
+        }
+
+        if (dphInput == false)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow) || dph > 0)
+            {
+                selectNumber--;
+                dphInput = true;
+            }
+            if (Input.GetKeyDown(KeyCode.DownArrow) || dph < 0)
+            {
+                selectNumber++;
+                dphInput = true;
+            }
+        }
+
+        if (selectNumber > 3)
+        {
+            selectNumber = 1;
+        }
+        if (selectNumber < 1)
+        {
+            selectNumber = 3;
+        }
+
+        //もう一回選択中
+        if (selectNumber == 1)
+        {
+            //選択中画像の色を白に
+            selectImage[0].color = Color.white;
+
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                SceneManager.LoadScene("CharacterSelect");
+            }
+        }
+        else
+        {
+            selectImage[0].color = Color.gray;
+        }
+
+        //次選択中
+        if (selectNumber == 2)
+        {
+            //選択中画像の色を白に
+            selectImage[1].color = Color.white;
+
+            //エンターキーを押したときの処理
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                //現在のステージによって、次で向かうステージを指定
+                switch (GameController.Instance.stage)
+                {
+                    case 0:
+                        GameController.Instance.stage = 1;
+                        break;
+                    case 1:
+                        GameController.Instance.stage = 2;
+                        break;
+                    case 2:
+                        GameController.Instance.stage = 3;
+                        break;
+                    case 3:
+                        GameController.Instance.stage = 4;
+                        break;
+                    case 4:
+                        GameController.Instance.stage = 5;
+                        break;
+                    case 5:
+                        GameController.Instance.stage = 6;
+                        break;
+                    case 6:
+                        GameController.Instance.stage = 7;
+                        break;
+                    case 7:
+                        GameController.Instance.stage = 8;
+                        break;
+                    case 8:
+                        GameController.Instance.stage = 9;
+                        break;
+                    case 9:
+                        GameController.Instance.stage = 10;
+                        break;
+                    case 10:
+                        GameController.Instance.stage = 11;
+                        break;
+                    case 11:
+                        GameController.Instance.stage = 12;
+                        break;
+                }
+
+                //キャラクター選択画面に遷移
+                SceneManager.LoadScene("CharacterSelect");
+            }
+        }
+        else
+        {
+            selectImage[1].color = Color.gray;
+        }
+
+        //マップ選択中
+        if (selectNumber == 3)
+        {
+            //選択中画像の色を白に
+            selectImage[2].color = Color.white;
+
+            //エンターキーでステージセレクト画面に遷移
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                SceneManager.LoadScene("StageSelect");
+            }
+        }
+        else
+        {
+            selectImage[2].color = Color.gray;
+        }
+
+        /*
         if (this)
         {
             //クリア表示後に、エンターキーでステージ選択画面に遷移
@@ -42,5 +178,6 @@ public class Clear : MonoBehaviour
                 Time.timeScale = 1.0f;
             }
         }
+        */
     }
 }
